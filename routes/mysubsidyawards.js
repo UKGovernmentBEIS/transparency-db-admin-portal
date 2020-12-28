f390e12ff255e0566ad969cfd61d9723948c9346
Itankar <2008389@cognizant.com>
2020-12-28 19:41:02 +0530

    UI alignment for search award and subsidy measure

--- routes/mysubsidyawards.js
+++ routes/mysubsidyawards.js
@@ -1,120 +1,115 @@
-
-
 // ********************************************************************
 // Gov.UK transparency subsidy awards routing module
 // ********************************************************************
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-var request = require('request');
+const axios = require("axios");
+var request = require("request");
 
-router.get('/',async(req, res) => {
+router.get("/", async (req, res) => {
+  frontend_totalRecordsPerPage = 10;
 
- 
-  frontend_totalRecordsPerPage = 1;
-
-  const data_request = 
-  {
-    "beneficiaryName": "",
-    "subsidyMeasureTitle": "",
-    "subsidyObjective": [],
-    "spendingRegion": [],
-    "subsidyInstrument": [],
-    "spendingSector":[],
-    "legalGrantingFromDate" :"",
-    "legalGrantingToDate" : "",
-    "pageNumber": 1,
-    "totalRecordsPerPage" : frontend_totalRecordsPerPage,
-    "sortBy" : [""]
-  
-};
- 
+  const data_request = {
+    beneficiaryName: "",
+    subsidyMeasureTitle: "",
+    subsidyObjective: [],
+    spendingRegion: [],
+    subsidyInstrument: [],
+    spendingSector: [],
+    legalGrantingFromDate: "",
+    legalGrantingToDate: "",
+    pageNumber: 1,
+    totalRecordsPerPage: frontend_totalRecordsPerPage,
+    sortBy: [""],
+  };
 
   var data = JSON.parse(JSON.stringify(data_request));
- 
+
   console.log("request :" + JSON.stringify(data));
-    
-      try {
-          const apidata = await axios.post('http://subsidy-search-service.azurewebsites.net/searchResults', data);
-          console.log(`Status: ${apidata.status}`);
-          API_response_code = `${apidata.status}`;
-          console.log("API_response_code: try" + API_response_code);
-          console.log('Body: ', apidata.data);
-          searchawards = apidata.data
-          var searchawards_api = apidata.data;
-          console.log("searchawards" + searchawards_api );
-          const seachawardstring = JSON.stringify(searchawards_api )
-          const seachawardJSON = JSON.parse(seachawardstring );
-          totalrows = searchawards.totalSearchResults;
-          console.log(searchawards.awards[0].subsidyFullAmountExact);  
-          
-          pageCount = Math.ceil(totalrows/frontend_totalRecordsPerPage );
-          console.log("totalrows :" + totalrows)
-          console.log("pageCount :" + pageCount)
-          curren_page = 1;
-          previous_page = 1;
-          next_page = 2;
-          start_record = 1;
-          end_record = frontend_totalRecordsPerPage;
-          current_page_active = 1;
-         
-          start_page = 1;
-          if(pageCount < 10) { end_page = pageCount}
-          else {
-            end_page = 9;
-          }
-          res.render('bulkupload/mysubsidyawards',{pageCount,previous_page,next_page,
-            start_record ,end_record,totalrows,current_page_active, frontend_totalRecordsPerPage   })
-          
-      }
-      
-      catch (err) {
 
-        response_error_message = err;
-        console.log("message error : " + err);
-        console.log("response_error_message catch : " + response_error_message );
-     
-        pageCount = 10;
-        previous_page = 1;
-        next_page = 2;
-        start_record = 1;
-        end_record = 9;
-        totalrows = 10;
-        current_page_active = 1;
-        current_page = 1;
-        start_page = 1;
-        end_page = 10;
+  try {
+    const apidata = await axios.post(
+      "http://subsidy-search-service.azurewebsites.net/searchResults",
+      data
+    );
+    console.log(`Status: ${apidata.status}`);
+    API_response_code = `${apidata.status}`;
+    console.log("API_response_code: try" + API_response_code);
+    console.log("Body: ", apidata.data);
+    searchawards = apidata.data;
+    var searchawards_api = apidata.data;
+    console.log("searchawards" + searchawards_api);
+    const seachawardstring = JSON.stringify(searchawards_api);
+    const seachawardJSON = JSON.parse(seachawardstring);
+    totalrows = searchawards.totalSearchResults;
+    console.log(searchawards.awards[0].subsidyFullAmountExact);
 
+    pageCount = Math.ceil(totalrows / frontend_totalRecordsPerPage);
+    console.log("totalrows :" + totalrows);
+    console.log("pageCount :" + pageCount);
+    curren_page = 1;
+    previous_page = 1;
+    next_page = 2;
+    start_record = 1;
+    end_record = frontend_totalRecordsPerPage;
+    current_page_active = 1;
 
-        res.render('bulkupload/mysubsidyawards',{pageCount,previous_page,
-            next_page,end_record ,end_record,totalrows,current_page_active,start_page,end_page, frontend_totalRecordsPerPage  })
-          
-        // response_error_message = err;
-        // console.log("message error : " + err);
-        // console.log("response_error_message catch : " + response_error_message );
-        // res.render('publicusersearch/noresults'); 
-      }
+    start_page = 1;
+    if (pageCount < 10) {
+      end_page = pageCount;
+    } else {
+      end_page = 9;
+    }
+    res.render("bulkupload/mysubsidyawards", {
+      pageCount,
+      previous_page,
+      next_page,
+      start_record,
+      end_record,
+      totalrows,
+      current_page_active,
+      frontend_totalRecordsPerPage,
+    });
+  } catch (err) {
+    response_error_message = err;
+    console.log("message error : " + err);
+    console.log("response_error_message catch : " + response_error_message);
+
+    pageCount = 10;
+    previous_page = 1;
+    next_page = 2;
+    start_record = 1;
+    end_record = 9;
+    totalrows = 10;
+    current_page_active = 1;
+    current_page = 1;
+    start_page = 1;
+    end_page = 10;
+
+    res.render("bulkupload/mysubsidyawards", {
+      pageCount,
+      previous_page,
+      next_page,
+      end_record,
+      end_record,
+      totalrows,
+      current_page_active,
+      start_page,
+      end_page,
+      frontend_totalRecordsPerPage,
+    });
+
+    // response_error_message = err;
+    // console.log("message error : " + err);
+    // console.log("response_error_message catch : " + response_error_message );
+    // res.render('publicusersearch/noresults');
+  }
 
   // end of POST call
-    });  
-
-
+});
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // old my subsidy award code for validation email and pass from home screen
 // **************************************************************************
@@ -123,10 +118,9 @@ module.exports = router;
 // const router = express.Router();
 
 // router.get('/',(req, res) => {
-  
+
 //     res.render('bulkupload/mysubsidyawards')
 //   });
-  
 
 //   router.post('/', (req, res) => {
 //     const { email_address , password} = req.body;
@@ -134,24 +128,22 @@ module.exports = router;
 //     let isPasswordEmpty = false;
 //     let isPasswordlesseight = false;
 
-    
-  
-//     if(!email_address && !password ) 
-//     { 
+//     if(!email_address && !password )
+//     {
 //       isEmailAndPasswordEmpty = true ;
 //     }
-  
+
 //     else if (!email_address) {
 //       isEmailAndPasswordEmpty = true ;
-  
+
 //     }
-                   
-//     else if(!password ) 
+
+//     else if(!password )
 //     {
 //     isPasswordEmpty = true;
 //     }
-       
-//     else if(password ) 
+
+//     else if(password )
 //     {
 //       if( password.length <8 ){
 
@@ -160,10 +152,9 @@ module.exports = router;
 
 //     }
 
-
-//     if (isEmailAndPasswordEmpty || isPasswordEmpty || isPasswordlesseight) 
+//     if (isEmailAndPasswordEmpty || isPasswordEmpty || isPasswordlesseight)
 //     {
-      
+
 //         res.render('accessmanagement/loginfirstpage', {
 //          isEmailAndPasswordEmpty ,
 //          isPasswordEmpty,
@@ -171,19 +162,15 @@ module.exports = router;
 //          email_address : email_address,
 //          password
 //          } );
-//     } 
+//     }
 //     // this is for password expiry page temporary
 //     else if (email_address =='xyz.xyz@xyz.com') {
 //       let isEmailValid = true;
 //       res.render('accessmanagement/loginresetpassword',{isEmailValid})
-   
+
 //     }
 //     else { res.render('bulkupload/mysubsidyawards')}
-  
+
 //     } );
-  
 
 // module.exports = router;
-
-
-
--- routes/mysubsidymeasures.js
+++ routes/mysubsidymeasures.js
@@ -1,99 +1,106 @@
-
 // ********************************************************************
 // Gov.UK public user search results routing module
 // ********************************************************************
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-var request = require('request');
+const axios = require("axios");
+var request = require("request");
 
-router.get('/',async(req, res) => {
+router.get("/", async (req, res) => {
+  frontend_totalRecordsPerPage = 10;
 
- 
-  frontend_totalRecordsPerPage = 1;
-
-  const data_request = 
-  {
-    "beneficiaryName": "",
-    "subsidyMeasureTitle": "",
-    "subsidyObjective": [],
-    "spendingRegion": [],
-    "subsidyInstrument": [],
-    "spendingSector":[],
-    "legalGrantingFromDate" :"",
-    "legalGrantingToDate" : "",
-    "pageNumber": 1,
-    "totalRecordsPerPage" : frontend_totalRecordsPerPage,
-    "sortBy" : [""]
-  
-};
- 
+  const data_request = {
+    beneficiaryName: "",
+    subsidyMeasureTitle: "",
+    subsidyObjective: [],
+    spendingRegion: [],
+    subsidyInstrument: [],
+    spendingSector: [],
+    legalGrantingFromDate: "",
+    legalGrantingToDate: "",
+    pageNumber: 1,
+    totalRecordsPerPage: frontend_totalRecordsPerPage,
+    sortBy: [""],
+  };
 
   var data = JSON.parse(JSON.stringify(data_request));
- 
+
   console.log("request :" + JSON.stringify(data));
-    
-      try {
-          const apidata = await axios.post('http://subsidy-search-service.azurewebsites.net/searchResults', data);
-          console.log(`Status: ${apidata.status}`);
-          API_response_code = `${apidata.status}`;
-          console.log("API_response_code: try" + API_response_code);
-          console.log('Body: ', apidata.data);
-          searchawards = apidata.data
-          var searchawards_api = apidata.data;
-          console.log("searchawards" + searchawards_api );
-          const seachawardstring = JSON.stringify(searchawards_api )
-          const seachawardJSON = JSON.parse(seachawardstring );
-          totalrows = searchawards.totalSearchResults;
-          console.log(searchawards.awards[0].subsidyFullAmountExact);  
-          
-          pageCount = Math.ceil(totalrows/frontend_totalRecordsPerPage );
-          console.log("totalrows :" + totalrows)
-          console.log("pageCount :" + pageCount)
-          curren_page = 1;
-          previous_page = 1;
-          next_page = 2;
-          start_record = 1;
-          end_record = frontend_totalRecordsPerPage;
-          current_page_active = 1;
-         
-          start_page = 1;
-          if(pageCount < 10) { end_page = pageCount}
-          else {
-            end_page = 9;
-          }
-          res.render('bulkupload/mysubsidymeasures',{pageCount,previous_page,next_page,
-            start_record ,end_record,totalrows,current_page_active   })
-          
-      }
-      
-      catch (err) {
-     
-        pageCount = 10;
-        previous_page = 1;
-        next_page = 2;
-        start_record = 1;
-        end_record = 9;
-        totalrows = 10;
-        current_page_active = 1;
-        current_page = 1;
-        start_page = 1;
-        end_page = 10;
 
+  try {
+    const apidata = await axios.post(
+      "http://subsidy-search-service.azurewebsites.net/searchResults",
+      data
+    );
+    console.log(`Status: ${apidata.status}`);
+    API_response_code = `${apidata.status}`;
+    console.log("API_response_code: try" + API_response_code);
+    console.log("Body: ", apidata.data);
+    searchawards = apidata.data;
+    var searchawards_api = apidata.data;
+    console.log("searchawards" + searchawards_api);
+    const seachawardstring = JSON.stringify(searchawards_api);
+    const seachawardJSON = JSON.parse(seachawardstring);
+    totalrows = searchawards.totalSearchResults;
+    console.log(searchawards.awards[0].subsidyFullAmountExact);
 
-        res.render('bulkupload/mysubsidymeasures',{pageCount,previous_page,
-            next_page,end_record ,end_record,totalrows,current_page_active,start_page,end_page   })
-          
-        // response_error_message = err;
-        // console.log("message error : " + err);
-        // console.log("response_error_message catch : " + response_error_message );
-        // res.render('publicusersearch/noresults'); 
-      }
+    pageCount = Math.ceil(totalrows / frontend_totalRecordsPerPage);
+    console.log("totalrows :" + totalrows);
+    console.log("pageCount :" + pageCount);
+    curren_page = 1;
+    previous_page = 1;
+    next_page = 2;
+    start_record = 1;
+    end_record = frontend_totalRecordsPerPage;
+    current_page_active = 1;
+
+    start_page = 1;
+    if (pageCount < 10) {
+      end_page = pageCount;
+    } else {
+      end_page = 9;
+    }
+    res.render("bulkupload/mysubsidymeasures", {
+      pageCount,
+      previous_page,
+      next_page,
+      start_record,
+      end_record,
+      totalrows,
+      current_page_active,
+    });
+  } catch (err) {
+    pageCount = 10;
+    previous_page = 1;
+    next_page = 2;
+    start_record = 1;
+    end_record = 9;
+    totalrows = 10;
+    current_page_active = 1;
+    current_page = 1;
+    start_page = 1;
+    end_page = 10;
+
+    res.render("bulkupload/mysubsidymeasures", {
+      pageCount,
+      previous_page,
+      next_page,
+      end_record,
+      end_record,
+      totalrows,
+      current_page_active,
+      start_page,
+      end_page,
+    });
+
+    // response_error_message = err;
+    // console.log("message error : " + err);
+    // console.log("response_error_message catch : " + response_error_message );
+    // res.render('publicusersearch/noresults');
+  }
 
   // end of POST call
-    });  
-
-
+});
 
 module.exports = router;
