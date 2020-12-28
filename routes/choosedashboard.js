1e6844b853e3de3527fed18fc7a8c36d9f090522
Maharajan <123502@cognizant.com>
2020-12-28 13:41:20 +0000

    api

--- routes/choosedashboard.js
+++ routes/choosedashboard.js
@@ -1,7 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+var request = require('request');
 
-router.post("/", (req, res) => {
+
+router.post("/", async(req, res) => {
 
 // *******************
 // Globale declarations
@@ -20,7 +23,43 @@ console.log("dashboard_roles:" + dashboard_roles);
 
 dashboard_user_name = dashboard_username;
 
-if ( dashboard_username == 'Albert Einstein') { res.render("bulkupload/dashboard-beisadmin");}
+if ( dashboard_username == 'Albert Einstein') { 
+
+
+// userPrinciple = 'userPrinciple';
+const userPrincipleRequest = '{"userName": "TEST","password": "password123","role": "BEIS Administrator","grantingAuthorityGroupId": "123","grantingAuthorityGroupName": "test"}';
+var config = {
+    headers: { 'userPrinciple': userPrincipleRequest }
+  
+ };
+
+//  var config = {
+//     headers: { 'userPrinciple':'{"userName": "TEST","password": "password123","role": "BEIS Administrator","grantingAuthorityGroupId": "123","grantingAuthorityGroupName": "test"}' }
+//  };
+  
+const userPrincipleRequest1 = {"userName": "TEST","password": "password123","role": "BEIS Administrator","grantingAuthorityGroupId": "123","grantingAuthorityGroupName": "test"}
+
+var data = JSON.parse(JSON.stringify(userPrincipleRequest));
+console.log("request :" + JSON.stringify(data));
+    
+      try {
+          const apidata = await axios.get('https://access-management-service.azurewebsites.net/accessmanagement/beisadmin', config);;
+          console.log(`Status: ${apidata.status}`);
+          API_response_code = `${apidata.status}`;
+          console.log("API_response_code: try" + API_response_code);
+          console.log('Body: ', apidata.data);
+          res.render("bulkupload/dashboard-beisadmin");          
+      }
+      
+      catch (err) {
+
+        response_error_message = err;
+        console.log("message error : " + err);
+        console.log("response_error_message catch : " + response_error_message );
+     
+      }
+
+}
 else if( dashboard_username == 'Barry Smith') { res.render("bulkupload/dashboard-gaadmin");}
 else if( dashboard_username == 'Claire Jones') { res.render("bulkupload/dashboard-gaapprover");}
 else if( dashboard_username == 'Mary Johnson') { res.render("bulkupload/dashboard-gaencoder");}
--- routes/dashboard-beisadmin.js
+++ routes/dashboard-beisadmin.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  res.render("bulkupload/dashboard-beisadmin");
+router.post("/", async(req, res) => {
+
+
+const userPrinciple = {"userName": "TEST","password": "password123","role": "BEIS Administrator","grantingAuthorityGroupId": "123","grantingAuthorityGroupName": "test"}
+var data = JSON.parse(JSON.stringify(userPrinciple));
+console.log("request :" + JSON.stringify(data));
+    
+      try {
+          const apidata = await axios.post('http://subsidy-search-service.azurewebsites.net/searchResults', data);
+          console.log(`Status: ${apidata.status}`);
+          API_response_code = `${apidata.status}`;
+          console.log("API_response_code: try" + API_response_code);
+          console.log('Body: ', apidata.data);
+          res.render("bulkupload/dashboard-beisadmin");          
+      }
+      
+      catch (err) {
+
+        response_error_message = err;
+        console.log("message error : " + err);
+        console.log("response_error_message catch : " + response_error_message );
+     
+      }
+  
 });
 
 module.exports = router;
