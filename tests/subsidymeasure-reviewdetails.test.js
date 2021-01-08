8143bb4ae1651eb96382d54c5d47397290cc9cb3
Itankar <2008389@cognizant.com>
2021-01-08 16:26:24 +0530

    subsidy measure edit and review

---
+++ tests/subsidymeasure-reviewdetails.test.js
@@ -0,0 +1,28 @@
+// ***********************************************************
+// Automated Unit testing scripts for search results award route
+// ***********************************************************
+
+const index = require("../app");
+const request = require("supertest");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", index);
+
+const mockRequest = (sessionData, body) => ({
+  session: { data: sessionData },
+  body,
+});
+
+test("Unit testing for Subsidy Scheme Review Test for GET call", (done) => {
+  const req = mockRequest();
+  const res = {};
+
+  request(app)
+    .get("/reviewsubsidymeasures", (req, res))
+    .expect(200, done);
+  //   expect(acd).toBe(200);
+});
