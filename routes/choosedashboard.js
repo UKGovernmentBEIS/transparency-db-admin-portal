aac8a0f700816827634a2e6dfa5fa2625e5cb54f
priyatham-bhikkumalla-cognizant <priyatham.bhikkumalla@cognizant.com>
2021-01-27 14:23:09 +0000

    dynamic role based dashboard

--- routes/choosedashboard.js
+++ routes/choosedashboard.js
@@ -1,4 +1,3 @@
-import jwt_decode from "jwt-decode";
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
@@ -61,8 +60,16 @@ router.post("/", async (req, res) => {
   var id_token = req.body.id_token;
   console.log("id_token "+id_token);
   var id_token_decoded = jwt_decode(id_token);
-  console.log("id_token_decoded "+id_token_decoded);
-  console.log("roles "+JSON.parse(id_token_decoded).roles);
+
+  if(id_token_decoded.roles.includes("4aaddb97-dcb8-4988-b2e5-b045a4419d90")){
+    dashboard_roles = "BEIS Administrator";
+  }else if(id_token_decoded.roles.includes("3ee46dda-5f2b-4fd5-b92b-54c2cd8f2930")){
+    dashboard_roles = "Granting Authority Administrator";
+  }else if(id_token_decoded.roles.includes("058abc1f-c491-4ffa-bd52-885c4fb96943")){
+    dashboard_roles = "Granting Authority Approver";
+  }else if(id_token_decoded.roles.includes("e7f70439-02d4-4367-817e-52283a416ac3")){
+    dashboard_roles = "Granting Authority Encoder";
+  }
 
   if (dashboard_roles == "BEIS Administrator") {
     const userPrincipleRequest =
