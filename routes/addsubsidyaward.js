b788b7c1775e224c7395a3784cd40c14bb7ea9b6
Itankar <2008389@cognizant.com>
2021-03-24 16:20:03 +0530

    subsidy award js

--- routes/addsubsidyaward.js
+++ routes/addsubsidyaward.js
@@ -6,8 +6,8 @@ router.get("/", (req, res) => {
   ssn = req.session;
   req.query = JSON.parse(JSON.stringify(req.query));
   if (req.query.hasOwnProperty("scheme"))
-    ssn.Subsidy_Control_Number_Global = req.query.scheme;
-  else ssn.Subsidy_Control_Number_Global = "";
+    ssn.Subsidy_Control_Number_Name_Global = req.query.scheme;
+  else ssn.Subsidy_Control_Number_Name_Global = "";
   ssn.Subsidy_Measure_Title_Global = "";
   ssn.Subsidy_Adhoc_Global = "";
   ssn.Subsidy_Objective_Global = "";
