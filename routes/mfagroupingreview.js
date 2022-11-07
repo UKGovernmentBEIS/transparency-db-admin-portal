da88dd4b945b9538f92219f396014686fb9df1ac
James Holder <james.holder@cgi.com>
2022-11-07 08:13:26 +0000

    Fix incorrect render

--- routes/mfagroupingreview.js
+++ routes/mfagroupingreview.js
@@ -169,7 +169,7 @@ router.get("/", (req, res) => {
     res.set("Access-Control-Allow-Origin", beis_url_accessmanagement);
     res.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
 
-    res.render("bulkupload/reviewdetail");
+    res.render("mfa/mfagroupingreviewdetail");
   }
 });
 
