e6099ba4b7d5cfe92b030d2e2b2325b822fcd1d7
RyanHumphreys00 <ryan.humphreys@cgi.com>
2024-05-30 11:37:56 +0100

    feat: Added ability to give a reason when deactivating/deleting schemes

--- routes/subsidymeasure-deactivate.js
+++ routes/subsidymeasure-deactivate.js
@@ -19,8 +19,41 @@ router.get("/", async (req, res) => {
     res.set("Content-Security-Policy", 'frame-ancestors "self"');
     res.set("Access-Control-Allow-Origin", beis_url_accessmanagement);
     res.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
-    res.render("bulkupload/subsidymeasure-deactivate");
+    if(req.query.hasOwnProperty("action"))
+    {
+      action = req.query.action;
+    }
+    else
+    {
+      action = "Deactivate";
+    }
+
+    if (ssn.dashboard_roles != "BEIS Administrator") {
+      res.render("bulkupload/notAuthorized");
+    }
+    charLimit = 1000;
+    
+    res.render("bulkupload/subsidymeasure-deactivate", {action, charLimit});
   }
 });
 
+
+router.post("/", (req, res) => {
+  ssn = req.session;
+  if (
+    typeof ssn.dashboard_roles_object_id1 === "undefined" ||
+    typeof ssn.dashboard_roles_object_id2 === "undefined"
+  ) {
+    res.redirect("/signout");
+  } else {
+    res.set("X-Frame-Options", "DENY");
+    res.set("X-Content-Type-Options", "nosniff");
+    res.set("Content-Security-Policy", 'frame-ancestors "self"');
+    res.set("Access-Control-Allow-Origin", beis_url_accessmanagement);
+    res.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+    reason = req.body.reason;
+    action = req.body.action;
+    res.render("bulkupload/subsidymeasure-deactivated-successfully",{action});
+  }
+});
 module.exports = router;
